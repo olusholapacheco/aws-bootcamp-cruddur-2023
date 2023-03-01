c97b71169a38e9c739ca922f82133bf0ee472b2d
olusholapacheco Pacheco <sholapacheco@gmail.com>
2023-03-01 16:38:36 +0000

    Instruments honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -44,6 +44,9 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+
+
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
